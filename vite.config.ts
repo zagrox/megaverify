7caa18305d947d5ec75b2c9074ef17ea650d5339
ZAGROX <zagrox@gmail.com>
2025-07-31 14:32:31 +0330

    vite new configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+})
+
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
